9fcc1bc926ff587b542b680256ccdc2f6a04d4a0
Geoff Hulette <geoff@hulette.net>
2025-07-01 10:26:04 -0700

    Cleanup

--- app/main.ml
+++ app/main.ml
@@ -1,13 +1,10 @@
 open Containers
 open Printf
-
 open Result.Infix
 
 let main =
-    Ocaml_starter.Lib.name () >>= fun greet ->
-    printf "Hello, %s!\n" greet;
-    Result.Ok 0
-    
-let _ =
-    main >>= fun ret ->
-    exit ret
+  Ocaml_starter.Lib.name () >>= fun greet ->
+  printf "Hello, %s!\n" greet;
+  Result.Ok 0
+
+let _ = main >>= fun ret -> exit ret
--- test/test_ocaml_starter.ml
+++ test/test_ocaml_starter.ml
@@ -2,8 +2,10 @@ open Containers
 open OUnit2
 open Ocaml_starter
 
-let tests = "test suite for library" >::: [
-    "greeting" >:: (fun _ -> assert_equal (Result.Ok "world") (Lib.name ()));
-]
+let tests =
+  "test suite for library"
+  >::: [
+         ("greeting" >:: fun _ -> assert_equal (Result.Ok "world") (Lib.name ()));
+       ]
 
 let _ = run_test_tt_main tests
